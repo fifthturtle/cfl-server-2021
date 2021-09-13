f255fe30a10d292f7235d14ecdc482c8aeda025c
Josh DeFamio <josh@fifthturtle.com>
2021-09-12 20:19:31 -0700

    login done through sockets

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const cors = {
 };
 const socket = require('./lib/socket');
 const _ = require('lodash');
-const ls = require('./lib/livescoring');
+//const ls = require('./lib/livescoring');
 
 function getAddress() {
   return new Promise((resolve, reject) => {
@@ -261,7 +261,7 @@ const init = async () => {
   await server.start();
   console.log("Server running on %s", server.info.uri);
   socket.connect(server.listener);
-  setTimeout(ls.init, 2000);
+  //setTimeout(ls.init, 2000);
 };
 
 process.on("unhandledRejection", (err) => {
